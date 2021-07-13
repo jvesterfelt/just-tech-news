94b4fc5c9e584e807ec8059294683cd55757ab59
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-07-13 16:11:32 -0600

    Finished the login route

--- models/User.js
+++ models/User.js
@@ -3,7 +3,11 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init({
@@ -33,30 +37,21 @@ User.init({
             len: [4]
         }
     },
+}, {
     hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
         async beforeCreate(newUserData) {
             newUserData.password = await bcrypt.hash(newUserData.password, 10);
             return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
         }
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user'
-}, {
-    // TABLE CONFIGURATION OPTIONS GO HERE
-
-    // pass in our imported sequelize connection
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing
-    underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: 'user'
 });
 
