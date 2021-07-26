b8acca02f29c22d4701466d8380b05611588c5fd
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-07-26 14:34:43 -0600

    Authguarded all non 'get' routes. Completed application.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const { Post, User, Vote, Comment } = require('../../models');
 const { route } = require('./user-routes');
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // get all users
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     console.log('==========');
     Post.findAll({
             attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
@@ -38,7 +39,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', withAuth, (req, res) => {
     Post.findOne({
             where: {
                 id: req.params.id
@@ -81,7 +82,7 @@ router.post('/', (req, res) => {
     Post.create({
             title: req.body.title,
             post_url: req.body.post_url,
-            user_id: req.body.user_id
+            user_id: req.session.user_id
         })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -95,12 +96,12 @@ router.put('/upvote', (req, res) => {
     // make sure the session exists first 
     if (req.session) {
         // pass session id along with all desctructured properties on req.body
-        Post.upvote({ ...req.body, user_id: req.session.user_id}, { Vote, Comment, User})
-        .then(updatedVoteData => res.json(updatedVoteData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
+        Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
     }
 });
 
